aa8a5455b8f658d04f49f2b4b63d6b306224d054
JRoy <joshroy126@gmail.com>
2020-02-01 17:58:07 -0500

    Add bongo mode

--- src/main/java/io/github/jroy/bongobot/BongoBot.java
+++ src/main/java/io/github/jroy/bongobot/BongoBot.java
@@ -28,13 +28,26 @@ public class BongoBot {
 
     //What mode are we in?
     Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("EST"));
-    if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY && calendar.get(Calendar.HOUR) == 1 && calendar.get(Calendar.AM_PM) == Calendar.PM) {
+    if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY && calendar.get(Calendar.HOUR) == 0 && calendar.get(Calendar.AM_PM) == Calendar.AM) {
       //Bongo Mode
+      List<Member> purges = collectMembers(guild);
+      log.sendMessage("yeah yeah whatup trevor here... bongo bot is bad so let's clean up the fuckin mess...").complete();
+      StringBuilder builder = new StringBuilder();
+      for (Member member : purges) {
+        if (builder.length() > 1500) {
+          botsShouting.sendMessage(builder.toString()).complete();
+          builder = new StringBuilder();
+        }
+        builder.append(member.getUser().getName()).append("#").append(member.getUser().getDiscriminator()).append("\n");
+        member.kick("Bongo purge").complete();
+      }
+      log.sendMessage("about " + purges.size() + " purples were just kicked...").complete();
+      log.sendMessage("lmao bye bye").complete();
     } else {
       //Debug Mode
       System.out.println("Debug Mode");
       List<Member> purges = collectMembers(guild);
-      botsShouting.sendMessage("Found " + purges.size() + " purgeable idiots.").complete();
+      botsShouting.sendMessage("Found " + purges.size() + " people who needs to up their pledge.").complete();
       StringBuilder builder = new StringBuilder();
       for (Member member : purges) {
         if (builder.length() > 1500) {
@@ -43,7 +56,7 @@ public class BongoBot {
         }
         builder.append(member.getAsMention()).append(" - ").append(member.getRoles().size()).append("\n");
       }
-      builder.append("**fuck you**");
+      builder.append("\n\n**better fucking resub or u guys about to get kicked on monday**");
       botsShouting.sendMessage(builder.toString()).complete();
     }
     jda.shutdown();
